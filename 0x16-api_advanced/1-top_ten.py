0d079f5cf792b939c846d53c69df43840b1131b8
bolexs <boluwatufeadeojo@gmail.com>
2023-11-08 16:13:23 +0100

    advanced api

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,23 +1,26 @@
 #!/usr/bin/python3
-"""Gather all data from an API"""
+"""Gather data from an API"""
 import requests
 
 
 def top_ten(subreddit):
-    """ Queries the Reddit API and returns the title of the first 10 posts"""
+    """
+    Queries the Reddit API and prints the titles of the first 10 hot posts
+    listed for a given subreddit
+    """
     url = f"https://www.reddit.com/r/{subreddit}/hot.json"
     headers = {'User-Agent': 'My user Agent 1.0'}
-    param = {'limit': 10}
+    params = {'limit': 10}
 
-
-    response = requests.get(url, headers=headers, param=param, allow_redirects=False)
+    response = requests.get(url, headers=headers, params=params,
+                            allow_redirects=False)
 
     if response.status_code == 200:
         data = response.json().get('data', {})
         children = data.get('children', [])
-        for kid in children:
-            upload = kid.get('data', {})
-            title = upload.get('title', '')
+        for child in children:
+            post = child.get('data', {})
+            title = post.get('title', '')
             print(title)
     else:
-        print('None')
+        print(None)
