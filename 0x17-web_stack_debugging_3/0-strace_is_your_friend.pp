59befe8cd8ad90d30f936b7400e155643d79a3bc
bolexs <boluwatufeadeojo@gmail.com>
2023-11-08 16:50:44 +0100

    strace is your friend

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,6 @@
 # Fix 500 server error when a GET request is made to the Apache web server
 
-exec {'replace':
-  provider => shell,
-  command => 'sed -i "s/phpp/php/g" /var/www/html/wp-settings.php'
+exec {'fix-wp':
+  command => 'sed -i "s/phpp/php/g" /var/www/html/wp-settings.php',
+  path => '/usr/local/bin/:/bin/'
 }
